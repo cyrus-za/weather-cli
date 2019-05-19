f77e7abd41d49b466f4dbfc67b80721780669c42
Pieter Venter <pieter@venter.pro>
2019-05-19 17:16:40 +0800

    fix failing test and add typings for openweathermap.org

--- src/weather.test.ts
+++ src/weather.test.ts
@@ -1,10 +1,29 @@
 import axios from 'axios';
 import weather, { WEATHER_API_URL } from './weather';
+import { WeatherResponses } from './types';
 
 jest.mock('axios');
 
 const mockedAxiosGet: jest.Mock<typeof axios.get> = (axios.get as any);
 
+const mockedResponse: WeatherResponses.CurrentWeatherDataResponse =
+  { // example data from openweathermap.org documentation
+    'coord': { 'lon': 139, 'lat': 35 },
+    'sys': { 'id': 123, type: 3, 'message': 1, 'country': 'JP', 'sunrise': 1369769524, 'sunset': 1369821049 },
+    'weather': [{ 'id': 804, 'main': 'clouds', 'description': 'overcast clouds', 'icon': '04n' }],
+    'main': { 'temp': 289.5, 'humidity': 89, 'pressure': 1013, 'temp_min': 287.04, 'temp_max': 292.04 },
+    'wind': { 'gust': 20, 'speed': 7.31, 'deg': 187.002 },
+    'clouds': { 'all': 92 },
+    'dt': 1369824698,
+    'id': 1851632,
+    'name': 'Shuzenji',
+    'cod': 200,
+    'base': 'base',
+    'visibility': 10
+  };
+
+// @ts-ignore
+mockedAxiosGet.mockResolvedValue({ data: mockedResponse });
 describe('weather.ts', () => {
 
   it('should import weather module', () => {
@@ -26,13 +45,6 @@ describe('weather.ts', () => {
   });
 
   it('should get weather response', async () => {
-    const mockedResponse = { // example data from openweathermap.org documentation
-      'weather': [{ 'id': 804, 'main': 'clouds', 'description': 'overcast clouds', 'icon': '04n' }],
-      'main': { 'temp': 289.5, 'humidity': 89, 'pressure': 1013, 'temp_min': 287.04, 'temp_max': 292.04 }
-
-    };
-    // @ts-ignore
-    mockedAxiosGet.mockResolvedValue({ data: mockedResponse });
     const response = await weather('');
     expect(response).toEqual('overcast clouds with 289.5 degrees Kelvin and a humidity of 89%');
   });
--- src/weather.ts
+++ src/weather.ts
@@ -1,12 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { WeatherResponses } from './types';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
 async function weather(location) {
-  await axios.get('/weather', {
+  const { data }: AxiosResponse<WeatherResponses.CurrentWeatherDataResponse> = await axios.get('/weather', {
     baseURL: WEATHER_API_URL,
     params: { q: location }
   });
+
+  return `${data.weather[0].description} with ${data.main.temp} degrees Kelvin and a humidity of ${data.main.humidity}%`;
 }
 
 export default weather;
