b6b069be9afbdb9123052c498c4f3878519785cc
Pieter Venter <pieter@venter.pro>
2019-05-19 20:15:46 +0800

    tests fixed for index.ts and tsconfig added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+// @ts-ignore
+import weather from './weather';
+
+const location = process.argv[2];
+
 async function main() {
-  console.log('');
+  const response = await weather(location);
+  console.log(`Weather in ${location} is currently ${response}`);
 }
 
-main().catch(console.error);
+main()
+  .catch((e: Error) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .then(() => {
+    process.exit();
+  });
