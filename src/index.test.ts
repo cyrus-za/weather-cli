2e6be62f749636aa7a204bc6b5a9f7c424942772
Pieter Venter <pieter@venter.pro>
2019-05-19 20:14:38 +0800

    index.ts tests added for command line (integration tests)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,27 @@
 import { exec } from 'child_process';
 import * as path from 'path';
+import { promisify } from 'util';
+import { API_KEY } from './weather';
+
+const pathToFile = path.join(__dirname, 'index.ts');
+
+const execAsync = promisify(exec);
 
 describe('index.ts', () => {
   it('should be able to be executed with ts-node without errors', () => {
-    const pathToFile = path.join(__dirname, 'index.ts');
     exec(`ts-node ${pathToFile}`);
+  });
+
+  it('should have open weather map api key in env', () => {
+    expect(API_KEY).toBeDefined();
+  });
+
+  it('should log the weather for New York', async () => {
+    const location = 'New York';
+    const { stdout, stderr }: any = await execAsync(`ts-node ${pathToFile} '${location}'`);
+    expect(stderr).toBeFalsy();
+    expect(stdout).toContain(`Weather in New York is currently `);
+    expect(stdout).toContain(` degrees Kelvin and a humidity of `);
+    expect(stdout.length).toBeGreaterThan(80);
   });
 });
